06537a8b1ce88f48eff6f3e4dde9283e83e15d8c
Juan Pablo Solano <juanpa_solano@hotmail.com>
2020-02-23 13:29:01 -0500

    list

--- packages/create-shopify-theme/tasks/init.js
+++ packages/create-shopify-theme/tasks/init.js
@@ -3,60 +3,81 @@ import execa from 'execa';
 import chalk from 'chalk';
 import os from 'os';
 import path from 'path';
+import Listr from 'listr';
 import validateProjectName from 'validate-npm-package-name';
 
 const checkName = name => {
-  const validationResult = validateProjectName(name);
-  if (!validationResult.validForNewPackages) {
-    console.error(
-      chalk.red(
-        `Cannot create a project named ${chalk.green(
-          `"${name}"`
-        )} because of npm naming restrictions:\n`
-      )
-    );
-    [
-      ...(validationResult.errors || []),
-      ...(validationResult.warnings || []),
-    ].forEach(error => {
-      console.error(chalk.red(`  * ${error}`));
-    });
-    console.error(chalk.red('\nPlease choose a different project name.'));
-    process.exit(1);
-  }
+  return new Promise((resolve, reject) => {
+    const validationResult = validateProjectName(name);
+    if (!validationResult.validForNewPackages) {
+      console.error(
+        chalk.red(
+          `Cannot create a project named ${chalk.green(
+            `"${name}"`
+          )} because of npm naming restrictions:\n`
+        )
+      );
+      [
+        ...(validationResult.errors || []),
+        ...(validationResult.warnings || []),
+      ].forEach(error => {
+        console.error(chalk.red(`  * ${error}`));
+      });
+      console.error(chalk.red('\nPlease choose a different project name.'));
+      reject(
+        new Error(
+          `Cannot create a project named "${name}" because of npm naming restrictions`
+        )
+      );
+    }
+    resolve();
+  });
 };
 
 export const init = async name => {
-  checkName(name);
+  const tasks = new Listr([
+    {
+      title: 'Check name',
+      task: () => checkName(name),
+    },
+    {
+      title: 'Crete folder and copy files',
+      task: () => {
+        if (fs.existsSync(name)) {
+          const msg = `The folder ${chalk.green(`"${name}"`)} already exists.`;
+          console.error(chalk.red(msg));
+          throw new Error(msg);
+        } else {
+          const root = path.resolve(name);
 
-  if (fs.existsSync(name)) {
-    console.error(
-      chalk.red(`The folder ${chalk.green(`"${name}"`)} already exists.`)
-    );
-    process.exit(1);
-  } else {
-    const root = path.resolve(name);
+          //Creates a new folder
+          fs.ensureDirSync(name);
 
-    //Creates a new folder
-    fs.ensureDirSync(name);
+          //Creates a package json in the new folder
+          const packageJson = {
+            name: name,
+            version: '0.1.0',
+            private: true,
+          };
+          fs.writeFileSync(
+            path.join(root, 'package.json'),
+            JSON.stringify(packageJson, null, 2) + os.EOL
+          );
 
-    //Creates a package json in the new folder
-    const packageJson = {
-      name: name,
-      version: '0.1.0',
-      private: true,
-    };
-    fs.writeFileSync(
-      path.join(root, 'package.json'),
-      JSON.stringify(packageJson, null, 2) + os.EOL
-    );
+          //Copies the files from the template
+          const templatePath = path.join(
+            require.resolve('../../cst-template/package.json', {
+              paths: [root],
+            }),
+            '..',
+            'template'
+          );
+          fs.copySync(templatePath, root);
+        }
+      },
+    },
+  ]);
+  await tasks.run();
 
-    //Copies the files from the template
-    const templatePath = path.join(
-      require.resolve('../../cst-template/package.json', { paths: [root] }),
-      '..',
-      'template'
-    );
-    fs.copySync(templatePath, root);
-  }
+  console.log(chalk.green('Project ready!'));
 };
